e2b5ae4baf11d3752e2952200e55e14923c07af8
Seif Abdelkader <saifm9633@gmail.com>
2025-11-11 21:54:09 +0200

    FIX Tailwind And ProfilePage(InProgress)

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,13 @@
+import ProfilePage from "./pages/profilePage.jsx";
+
+
+// function App() {
+//   return <h1 className="text-3xl font-bold text-red-500">Hello Tailwind!</h1>;
+// }
+// export default App;
+
 function App() {
-  return <h1 className="text-3xl font-bold text-red-500">Hello Tailwind!</h1>;
+  return <ProfilePage />;
 }
+
 export default App;
